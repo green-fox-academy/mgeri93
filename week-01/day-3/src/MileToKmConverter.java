8413e1ff0b470a96b7f63832d9c31cc38e38c982
mGeri <gerasz01@gmail.com>
2019-04-04 14:38:12 +0200

    MileToKmConverter

---
+++ week-01/day-3/src/MileToKmConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MileToKmConverter {
+    public static void main(String[] args) {
+        // Write a program that asks for an integer that is a distance in kilometers,
+        // then it converts that value to miles and prints it
+
+        double kmToMile = 0.621371192;
+
+        System.out.println("Please enter a distance in kilometers:");
+        Scanner scanner = new Scanner(System.in);
+        int userInput =  scanner.nextInt();
+
+        System.out.println(userInput * kmToMile);
+
+        // The title is misleading, it should be KmToMileConverter
+
+
+
+
+
+    }
+}
