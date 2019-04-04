76d620ca6855e94a3adfe9d5a695c673aa8a5562
mGeri <gerasz01@gmail.com>
2019-04-04 14:48:11 +0200

    Animals

--- week-01/day-3/src/AnimalsAndLegs.java
+++ week-01/day-3/src/AnimalsAndLegs.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class AnimalsAndLegs {
-    
+    public static void main(String[] args) {
+        // Write a program that asks for two integers
+        // The first represents the number of chickens the farmer has
+        // The second represents the number of pigs owned by the farmer
+        // It should print how many legs all the animals have
+
+        System.out.println("How many chickens does the farmer have?");
+        Scanner scanner1 = new Scanner(System.in);
+        int userInput1 =  scanner1.nextInt();
+
+        System.out.println("How many pigs does the farmer have?");
+        Scanner scanner2 = new Scanner(System.in);
+        int userInput2 =  scanner1.nextInt();
+
+        int allLegs = ((userInput1 * 2) + (userInput2 * 4));
+
+        System.out.println(("The animals have ") + (allLegs) + (" legs in total."));
+
+    }
 }
