378b8127f3c387c4fe6e56202bce26854b840e35
mGeri <gerasz01@gmail.com>
2019-04-26 09:30:09 +0200

    examStartingMatrix

---
+++ exam-trial-basics/src/SymmetricMatrix/SymmetricMatrix.java
@@ -0,0 +1,33 @@
+package SymmetricMatrix;
+
+public class SymmetricMatrix {
+/*  Create a function named isSymmetric that takes an n×n integer matrix (two dimensional array/list) as a parameter and returns true if the matrix is symmetric or false if it is not.
+
+  Symmetric means it has identical values along its diagonal axis from top-left to bottom-right, as in the first example.
+
+  for (int i = 0; i < a; i++) {
+      for (int j = 0; j < b; j++) {
+        if (i == j) {
+          matrix[i][j] = 1;
+          System.out.print(matrix[i][j]);
+        } else {
+          matrix[i][j] = 0;
+          System.out.print(matrix[i][j]);
+        }
+      }
+      System.out.println();
+    }
+*/
+
+public boolean isSymmetric (int matrix[][]){
+  if
+
+
+
+
+}
+
+
+}
+
+
