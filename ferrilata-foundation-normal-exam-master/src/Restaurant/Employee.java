b77883a0db214270809381a486ff71438dc7067d
mGeri <gerasz01@gmail.com>
2019-05-03 10:45:36 +0200

    createEmployeeAndRestaurant

--- ferrilata-foundation-normal-exam-master/src/Restaurant/Employee.java
+++ ferrilata-foundation-normal-exam-master/src/Restaurant/Employee.java
@@ -1,5 +1,14 @@
 package Restaurant;
 
-public class Employee {
+public abstract class Employee {
+
+  String name;
+  int experience = 0;
+
+  public Employee(String name) {
+    this.name = name;
+  }
+
+  public abstract void work();
 
 }
